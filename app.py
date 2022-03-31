430fca1b74c78c69ad1d901e006c18dfa29d9060
LRS4 <42714431+LRS4@users.noreply.github.com>
2022-03-31 18:09:16 +0100

    Disable "Updating..." on title

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import dash
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(
+    __name__, 
+    suppress_callback_exceptions=True,
+    update_title=None
+)
 
 server = app.server
