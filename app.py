cf662281696a717ce4efd14711f9d1ab04814ccf
LRS4 <42714431+LRS4@users.noreply.github.com>
2022-04-06 15:41:49 +0100

    Add viewport meta tags

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = dash.Dash(
     update_title=None,
     external_stylesheets=[
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
-    ]
+    ],
+    meta_tags=[{
+        "name": "viewport",
+        "content": "width=device-width, initial-scale=1.0"
+    }]
 )
 
 server = app.server
