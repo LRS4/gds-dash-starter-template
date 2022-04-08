0548e2dc348be258f984b5b3e04df2a85d519da9
LRS4 <42714431+LRS4@users.noreply.github.com>
2022-04-08 14:59:27 +0100

    Add initial headline cards with tooltips

--- section/headlines.py
+++ section/headlines.py
@@ -12,6 +12,114 @@ layout = html.Div(
             " number of avocados sold in the US"
             " between 2015 and 2018.",
             className="govuk-body"
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(
+                            children=[
+                                html.Strong(
+                                    children="Cases",
+                                    className="govuk-caption-m govuk-!-font-weight-regular"
+                                ),
+                                html.H4(
+                                    children="People tested positive",
+                                    className="govuk-heading-m title-mobile govuk-!-margin-bottom-0"
+                                ),
+                                html.Strong(
+                                    children="Last 7 days",
+                                    className="govuk-body-s float govuk-!-margin-top-3 govuk-!-margin-bottom-0"
+                                ),
+                                html.Div(
+                                    children=[
+                                        html.Div(
+                                            children=[
+                                                "361,774",
+                                                html.Span(
+                                                    children="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
+                                                    className="tooltiptext govuk-!-font-size-16"
+                                                )
+                                            ],
+                                            className="tooltip"
+                                        )
+                                    ],
+                                    className="govuk-heading-m govuk-!-margin-bottom-0 govuk-!-padding-top-0"
+                                )
+                            ],
+                            className="card-container"
+                        )
+                    ],
+                    className="govuk-grid-column-one-third"
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children=[
+                                html.Strong(
+                                    children="Cases",
+                                    className="govuk-caption-m govuk-!-font-weight-regular"
+                                ),
+                                html.H4(
+                                    children="People tested positive",
+                                    className="govuk-heading-m title-mobile govuk-!-margin-bottom-0"
+                                ),
+                                html.Strong(
+                                    children="Last 7 days",
+                                    className="govuk-body-s float govuk-!-margin-top-3 govuk-!-margin-bottom-0"
+                                ),
+                                html.Div(
+                                    children=[
+                                        html.Div(
+                                            children=[
+                                                "361,774",
+                                                html.Span(
+                                                    children="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
+                                                    className="tooltiptext govuk-!-font-size-16"
+                                                )
+                                            ],
+                                            className="tooltip"
+                                        )
+                                    ],
+                                    className="govuk-heading-m govuk-!-margin-bottom-0 govuk-!-padding-top-0"
+                                )
+                            ],
+                            className="card-container"
+                        )
+                    ],
+                    className="govuk-grid-column-one-third",
+                ),
+                html.Div(
+                    children=[
+                        "test"
+                    ],
+                    className="govuk-grid-column-one-third"
+                )
+            ],
+            className="govuk-grid-row"
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        "test"
+                    ],
+                    className="govuk-grid-column-one-third"
+                ),
+                html.Div(
+                    children=[
+                        "test"
+                    ],
+                    className="govuk-grid-column-one-third"
+                ),
+                html.Div(
+                    children=[
+                        "test"
+                    ],
+                    className="govuk-grid-column-one-third"
+                )
+            ],
+            className="govuk-grid-row govuk-!-margin-top-5"
         )
     ]
 )
