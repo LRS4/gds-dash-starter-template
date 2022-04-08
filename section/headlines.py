1461edbef808190a3fc7d392ec72c66dbc18d1a1
LRS4 <42714431+LRS4@users.noreply.github.com>
2022-04-08 16:11:29 +0100

    Update number card with optional change tag

--- section/headlines.py
+++ section/headlines.py
@@ -1,5 +1,6 @@
 from dash import dcc
 from dash import html
+from components.number_card import number_card
 
 layout = html.Div(
     children=[
@@ -17,81 +18,45 @@ layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        html.Div(
-                            children=[
-                                html.Strong(
-                                    children="Cases",
-                                    className="govuk-caption-m govuk-!-font-weight-regular"
-                                ),
-                                html.H4(
-                                    children="People tested positive",
-                                    className="govuk-heading-m title-mobile govuk-!-margin-bottom-0"
-                                ),
-                                html.Strong(
-                                    children="Last 7 days",
-                                    className="govuk-body-s float govuk-!-margin-top-3 govuk-!-margin-bottom-0"
-                                ),
-                                html.Div(
-                                    children=[
-                                        html.Div(
-                                            children=[
-                                                "361,774",
-                                                html.Span(
-                                                    children="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
-                                                    className="tooltiptext govuk-!-font-size-16"
-                                                )
-                                            ],
-                                            className="tooltip"
-                                        )
-                                    ],
-                                    className="govuk-heading-m govuk-!-margin-bottom-0 govuk-!-padding-top-0"
-                                )
-                            ],
-                            className="card-container"
+                        number_card(
+                            caption="Cases",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
+                            change_tag_label="-32.4%",
+                            change_tag_type="good",
+                            change_tag_arrow_direction="decrease",
+                            change_tag_tooltip_text="Change from previous return"
                         )
                     ],
                     className="govuk-grid-column-one-third"
                 ),
                 html.Div(
                     children=[
-                        html.Div(
-                            children=[
-                                html.Strong(
-                                    children="Cases",
-                                    className="govuk-caption-m govuk-!-font-weight-regular"
-                                ),
-                                html.H4(
-                                    children="People tested positive",
-                                    className="govuk-heading-m title-mobile govuk-!-margin-bottom-0"
-                                ),
-                                html.Strong(
-                                    children="Last 7 days",
-                                    className="govuk-body-s float govuk-!-margin-top-3 govuk-!-margin-bottom-0"
-                                ),
-                                html.Div(
-                                    children=[
-                                        html.Div(
-                                            children=[
-                                                "361,774",
-                                                html.Span(
-                                                    children="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
-                                                    className="tooltiptext govuk-!-font-size-16"
-                                                )
-                                            ],
-                                            className="tooltip"
-                                        )
-                                    ],
-                                    className="govuk-heading-m govuk-!-margin-bottom-0 govuk-!-padding-top-0"
-                                )
-                            ],
-                            className="card-container"
+                        number_card(
+                            caption="Test 2",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
+                            change_tag_label="+32.4%",
+                            change_tag_type="bad",
+                            change_tag_arrow_direction="increase",
+                            change_tag_tooltip_text="Change from previous return"
                         )
                     ],
                     className="govuk-grid-column-one-third",
                 ),
                 html.Div(
                     children=[
-                        "test"
+                        number_card(
+                            caption="Test 3",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                        )
                     ],
                     className="govuk-grid-column-one-third"
                 )
@@ -102,19 +67,37 @@ layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        "test"
+                        number_card(
+                            caption="Test 3",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                        )
                     ],
                     className="govuk-grid-column-one-third"
                 ),
                 html.Div(
                     children=[
-                        "test"
+                        number_card(
+                            caption="Test 3",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                        )
                     ],
                     className="govuk-grid-column-one-third"
                 ),
                 html.Div(
                     children=[
-                        "test"
+                        number_card(
+                            caption="Test 3",
+                            title="People tested positive",
+                            description="Last 7 days",
+                            number="361,774",
+                            tooltip_text="test"
+                        )
                     ],
                     className="govuk-grid-column-one-third"
                 )
