807ef8aec89b080d21f5032f149b73f2dbbb9a08
LRS4 <42714431+LRS4@users.noreply.github.com>
2022-05-04 15:56:53 +0100

    Populate number cards with headline data

--- section/headlines.py
+++ section/headlines.py
@@ -1,6 +1,56 @@
+import math
 from dash import dcc
 from dash import html
+import numpy as np
+import pandas as pd
+import pathlib
 from components.number_card import number_card
+
+path = pathlib.Path(__file__).parent
+data_path = path.joinpath("../data").resolve()
+data = pd.read_csv(data_path.joinpath("avocado.csv"))
+data["Date"] = pd.to_datetime(data["Date"])
+
+end_price = data["AveragePrice"].head(1).values[0]
+start_price = data["AveragePrice"].tail(1).values[0]
+
+data_excluding_big_regions = \
+    data[~data["region"].isin(
+        ["TotalUS", "West", "Southeast", "Northeast", "SouthCentral"])]
+
+top_sales_region = data_excluding_big_regions[["region", "Total Volume"]] \
+    .groupby("region") \
+    .max() \
+    .sort_values(by="Total Volume") \
+    .tail(1)
+
+bottom_sales_region = data_excluding_big_regions[["region", "Total Volume"]] \
+    .groupby("region") \
+    .max() \
+    .sort_values(by="Total Volume") \
+    .head(1)
+
+top_sales_type = data_excluding_big_regions[["type", "Total Volume"]] \
+    .groupby("type") \
+    .max() \
+    .sort_values(by="Total Volume") \
+    .tail(1)
+
+bottom_sales_type = data_excluding_big_regions[["type", "Total Volume"]] \
+    .groupby("type") \
+    .max() \
+    .sort_values(by="Total Volume") \
+    .head(1)
+
+headlines = {
+    "average_price": round(data["AveragePrice"].mean(), 2),
+    "average_price_change": round(((end_price - start_price) / start_price) * 100, 2),
+    "total_avocados_sold": f'{math.floor(data["Total Volume"].sum()):,}',
+    "region_with_highest_sales": top_sales_region,
+    "type_with_highest_sales": top_sales_type,
+    "region_with_lowest_sales": bottom_sales_region,
+    "type_with_lowest_sales": bottom_sales_type
+}
 
 layout = html.Div(
     children=[
@@ -19,15 +69,15 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Cases",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
+                            caption="Price",
+                            title="Average price",
+                            description="All time",
+                            number="$" + str(headlines["average_price"]),
                             tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
-                            change_tag_label="-32.4%",
-                            change_tag_type="good",
+                            change_tag_label=str(headlines["average_price_change"]) + "%",
+                            change_tag_type="bad",
                             change_tag_arrow_direction="decrease",
-                            change_tag_tooltip_text="Change from previous return"
+                            change_tag_tooltip_text="Change in price from 2015 to now"
                         )
                     ],
                     className="govuk-grid-column-one-third"
@@ -35,15 +85,15 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Test 2",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
-                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)",
+                            caption="Volume",
+                            title="Total sold",
+                            description="All time",
+                            number=headlines["total_avocados_sold"],
+                            tooltip_text="Total sales made between 2015 and 2018",
                             change_tag_label="+32.4%",
                             change_tag_type="bad",
                             change_tag_arrow_direction="increase",
-                            change_tag_tooltip_text="Change from previous return"
+                            change_tag_tooltip_text="Change from previous year"
                         )
                     ],
                     className="govuk-grid-column-one-third",
@@ -51,11 +101,15 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Test 3",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
-                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                            caption="Region",
+                            title="Top sales region",
+                            description=headlines["region_with_highest_sales"].index,
+                            number=f'{int(headlines["region_with_highest_sales"].values[0][0]):,}',
+                            tooltip_text="Total sales made in the top sales region",
+                            change_tag_label="+32.4%",
+                            change_tag_type="good",
+                            change_tag_arrow_direction="increase",
+                            change_tag_tooltip_text="Change from previous return"
                         )
                     ],
                     className="govuk-grid-column-one-third"
@@ -68,11 +122,11 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Test 3",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
-                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                            caption="Type",
+                            title="Top sales type",
+                            description=headlines["type_with_highest_sales"].index,
+                            number=f'{int(headlines["type_with_highest_sales"].values[0][0]):,}',
+                            tooltip_text="Total sales made by the top sales type"
                         )
                     ],
                     className="govuk-grid-column-one-third"
@@ -80,11 +134,11 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Test 3",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
-                            tooltip_text="Total number of people tested positive reported in the last 7 days (1 April 2022 - 7 April 2022)"
+                            caption="Region",
+                            title="Bottom sales region",
+                            description=headlines["region_with_lowest_sales"].index,
+                            number=f'{int(headlines["region_with_lowest_sales"].values[0][0]):,}',
+                            tooltip_text="Total sales made by the bottom sales region"
                         )
                     ],
                     className="govuk-grid-column-one-third"
@@ -92,11 +146,11 @@ layout = html.Div(
                 html.Div(
                     children=[
                         number_card(
-                            caption="Test 3",
-                            title="People tested positive",
-                            description="Last 7 days",
-                            number="361,774",
-                            tooltip_text="test"
+                            caption="Type",
+                            title="Bottom sales type",
+                            description=headlines["type_with_lowest_sales"].index,
+                            number=f'{int(headlines["type_with_lowest_sales"].values[0][0]):,}',
+                            tooltip_text="Total sales made by the bottom sales type"
                         )
                     ],
                     className="govuk-grid-column-one-third"
